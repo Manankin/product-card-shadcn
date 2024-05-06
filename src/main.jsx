8540cb0f3cededbd023d555808e34b0e4485429f
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-05-06 14:19:45 +0300

    init project

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+
+const element =document.querySelector('#root');
+createRoot(element).render(
+  <div>
+    <h1>My new Vite-React App</h1>
+  </div>
+);
